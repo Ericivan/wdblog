1c506524356d2f70c1662251e62a3eb32755ffd1
ericivan <ericivan047@gmail.com>
2018-03-26 22:05:01 +0800

    fix

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL数据库用户名 */
-define('DB_USER', getenv('DB_USER'));
+define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_PASSWORD', 'ericivan');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
